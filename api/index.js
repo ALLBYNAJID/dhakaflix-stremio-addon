764075580b0c8491e3a3cec2e8e8cccbcc43908f
PERFECTALL <najidpubglover12@gmail.com>
2025-06-10 23:32:44 +0600

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,14 @@
 const { getRouter } = require('stremio-addon-sdk');
-const addonInterface = require('../server');  // one level up, root folder
+const addonInterface = require('../server');
 
 const router = getRouter(addonInterface);
 
 module.exports = (req, res) => {
-  router(req, res);
+  router(req, res, (err) => {
+    if (err) {
+      console.error('Router error:', err);
+      res.statusCode = 500;
+      res.end('Internal Server Error');
+    }
+  });
 };
